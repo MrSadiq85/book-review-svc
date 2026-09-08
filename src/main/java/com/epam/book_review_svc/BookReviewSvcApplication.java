cb9735a9136e3738d81688cf81baa519a48981fe
MohdSadique <mohd_sadique@epam.com>
2026-09-08 13:13:43 +0530

    initialize git

---
+++ src/main/java/com/epam/book_review_svc/BookReviewSvcApplication.java
@@ -0,0 +1,13 @@
+package com.epam.book_review_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/book_review_svc/BookReviewSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.book_review_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
